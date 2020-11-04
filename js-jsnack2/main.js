9a01b1a9d72c5b67efdf36f175521241b8261aba
Daoud <99m.daoud@gmail.com>
2020-11-04 16:47:27 +0100

    Aggiunto metodo per non utilizzare variabile globale nella callback function

--- js-jsnack2/main.js
+++ js-jsnack2/main.js
@@ -5,10 +5,19 @@
 // Creazione array conenente una lista di nomi
 const nameArray = ['Pippo', 'Pluto', 'Paperino', 'Mario', 'Francesco', 'Luigi', 'Topolino'];
 
-const min = prompt('Inserire posizione iniziale array') - 1;
+const min = parseInt(prompt('Inserire posizione iniziale array')) - 1;
 
-const max = prompt('Inserire posizione finale array');
+const max = parseInt(prompt('Inserire posizione finale array'));
 
-const newNameArray = nameArray.filter((name, index) => (index >= min && index < max));
+// Utilizzando nella callback function una variabile globale
+// const newNameArray = nameArray.filter((name, index) => (index >= min && index < max));
+
+// Non utilizzando variabili globali nella callback function
+const filterFunction = (indexMin, indexMax) => {
+    console.log(indexMin, indexMax);
+    return (name, index) => (index >= indexMin && index < indexMax)
+}
+
+const newNameArray = nameArray.filter(filterFunction(min, max));
 
 console.log(nameArray, newNameArray);
