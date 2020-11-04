35ad403f1c995f2aaf88489ebe836db3c99929fa
Daoud <99m.daoud@gmail.com>
2020-11-04 16:34:16 +0100

    Aggiunto Jsnack 1 - 2

--- js-jsnack1/main.js
+++ js-jsnack1/main.js
@@ -1,3 +1,45 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa
 // con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+// Creazione arrow function che genera un numero random
+const getRndNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+// Creazione array di bici (oggetti) con peso casuale da 10 a 100
+const bikeList = [
+    {
+        name: 'pippoBike',
+        weightInKg: getRndNumber(10, 100)
+    },
+    {
+        name: 'plutoBike',
+        weightInKg: getRndNumber(10, 100)
+    },
+    {
+        name: 'paperinoBike',
+        weightInKg: getRndNumber(10, 100)
+    },
+    {
+        name: 'topolinoBike',
+        weightInKg: getRndNumber(10, 100)
+    }
+];
+
+// Dichiarazione variabile e inizializzazione come il primo elemento
+let lightestBike = bikeList[0];
+
+// Ciclo che confronta il peso di tut
+bikeList.forEach((thisBike) => {
+    let {name, weightInKg} = thisBike;
+    console.log(`Il peso della bici ${name} è ${weightInKg} Kg`);
+    if(weightInKg < lightestBike.weightInKg) {
+        lightestBike = thisBike;
+    }
+});
+
+let {name, weightInKg} = lightestBike;
+console.log(`
+    La bici più leggera è ${name}.
+    con un peso di ${weightInKg} Kg
+
+`);
--- js-jsnack2/main.js
+++ js-jsnack2/main.js
@@ -1,3 +1,14 @@
 // Creare un array di nomi e chiedere all'utente due numeri.
 // Creare un nuovo array con i valori che si trovano in una posizione compresa (compresi i numeri inseriti)
 // tra i due numeri inseriti dall'utente.
+
+// Creazione array conenente una lista di nomi
+const nameArray = ['Pippo', 'Pluto', 'Paperino', 'Mario', 'Francesco', 'Luigi', 'Topolino'];
+
+const min = prompt('Inserire posizione iniziale array') - 1;
+
+const max = prompt('Inserire posizione finale array');
+
+const newNameArray = nameArray.filter((name, index) => (index >= min && index < max));
+
+console.log(nameArray, newNameArray);
