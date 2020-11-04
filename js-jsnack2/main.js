1757409d5ac1ca349691c2f30b7ebe782e8eaad4
Daoud <99m.daoud@gmail.com>
2020-11-04 15:30:24 +0100

    Aggiunti file vuoti

---
+++ js-jsnack2/main.js
@@ -0,0 +1,3 @@
+// Creare un array di nomi e chiedere all'utente due numeri.
+// Creare un nuovo array con i valori che si trovano in una posizione compresa (compresi i numeri inseriti)
+// tra i due numeri inseriti dall'utente.
